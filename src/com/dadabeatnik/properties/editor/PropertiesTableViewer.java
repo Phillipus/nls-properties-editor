e565aa99bcb7696c84465ee0db40fd01c4003ab4
Phillipus <p.beauvoir@dadabeatnik.com>
2012-04-10 17:35:58 +0100

    Add licence and headers

--- src/com/dadabeatnik/properties/editor/PropertiesTableViewer.java
+++ src/com/dadabeatnik/properties/editor/PropertiesTableViewer.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2010, 2012 Phillip Beauvoir
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Phillip Beauvoir
+ *******************************************************************************/
 package com.dadabeatnik.properties.editor;
 
 import java.util.List;
